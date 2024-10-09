a6943a3f6ff134008aa98e3870598f4bcf93071d
lionson505 <143701468+lionson505@users.noreply.github.com>
2024-10-10 01:24:42 +0200

    index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://giranezajeandedieu2:ybP5tuy4TMg4Xa7f@cluster0.c7aff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://giranezajeandedieu2:ybP5tuy4TMg4Xa7f@cluster0.c7aff.mongodb.net/netcompassion?retryWrites=true&w=majority&appName=Cluster0";
 
 
 
