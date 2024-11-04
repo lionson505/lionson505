3a8486cb741b7cdba4c2905db11324d74099fdec
lionson505 <143701468+lionson505@users.noreply.github.com>
2024-11-05 01:09:05 +0200

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -35,52 +35,61 @@ app.use('/api/schoolFees', schoolFeesRoutes);
 app.use('/api/compassion', compassionRoutes);
 
 
-// ------------- Login --------------
+
+// ------------- Signin --------------
+let userAuthCheck;
 app.post("/api/login", async (req, res) => {
+  console.log(req.body);
   try {
-    const user = await User.findOne({ email: req.body.email });
-    if (user && await bcrypt.compare(req.body.password, user.password)) {
-      // Create a token
-      const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
-      res.json({ user, token });
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    console.log("USER: ", user);
+    if (user) {
+      res.send(user);
+      userAuthCheck = user;
     } else {
       res.status(401).send("Invalid Credentials");
+      userAuthCheck = null;
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    console.log(error);
+    res.status(500).send(error);
   }
 });
 
-// ------------- Registration API --------------
-app.post("/api/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const registerUser = new User({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: hashedPassword,
-      phoneNumber: req.body.phoneNumber,
-      imageUrl: req.body.imageUrl,
-      role: req.body.role,
-    });
-    const result = await registerUser.save();
-    res.status(201).send(result);
-  } catch (err) {
-    console.error("Signup Error: ", err);
-    res.status(500).send("Registration failed");
-  }
+// Getting User Details of logged-in user
+app.get("/api/login", (req, res) => {
+  res.send(userAuthCheck);
 });
 
-// Sample test route
-app.get("/testget", async (req, res) => {
-  try {
-    const result = await Product.findOne({ _id: '6429979b2e5434138eda1564' });
-    res.json(result);
-  } catch (error) {
-    res.status(500).send("Error fetching product");
-  }
+// Registration API
+app.post("/api/register", (req, res) => {
+  let registerUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    phoneNumber: req.body.phoneNumber,
+    imageUrl: req.body.imageUrl,
+    role: req.body.role,
+  });
+
+  registerUser
+    .save()
+    .then((result) => {
+      res.status(200).send(result);
+      alert("Signup Successfull");
+    })
+    .catch((err) => console.log("Signup: ", err));
+  console.log("request: ", req.body);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Start the server
