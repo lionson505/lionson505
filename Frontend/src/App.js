b58f0b1a0ae62a5486ffa6590de7221427c8d07e
lionson505 <143701468+lionson505@users.noreply.github.com>
2024-11-05 02:25:59 +0200

    App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -64,7 +64,7 @@ const App = () => {
                     >
                         <Route index element={<Dashboard />} />
 
-                        <Route path="stockreport" element={
+                        <Route path="stock" element={
                             <ProtectedWrapper allowedRoles={['burser','pd']}>
                                 <StockReport />
                             </ProtectedWrapper>
